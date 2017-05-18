9414c33a8a3206af224eb24b3be558b4875c1c75
Brandon Hubbard <bkhubbard@gmail.com>
2017-05-18 10:15:55 -0700

    WP Enforcer

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * Functions
+ *
+ * @package WP Background Processing
+ */
 
 if ( ! function_exists( 'wp_queue' ) ) {
 	/**
 	 * WP queue.
 	 *
-	 * @param WP_Job $job
-	 * @param int    $delay
+	 * @param WP_Job $job Job.
+	 * @param int    $delay Delay.
 	 */
 	function wp_queue( WP_Job $job, $delay = 0 ) {
 		global $wp_queue;
@@ -20,8 +25,8 @@ if ( ! function_exists( 'wp_queue_batch' ) ) {
 	/**
 	 * WP queue batch.
 	 *
-	 * @param array $jobs
-	 * @param int   $delay
+	 * @param array $jobs Jobs.
+	 * @param int   $delay Delay.
 	 */
 	function wp_queue_batch( $jobs, $delay = 0 ) {
 		foreach ( $jobs as $job ) {
